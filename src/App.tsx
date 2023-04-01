c203d58dbc5a2b5c5694f4af605d68fce9753436
youssefElMkhantar <youssef.mkht@gmail.com>
2023-04-01 09:14:29 +0200

    adding noteForm

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useMemo } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import { Container } from 'react-bootstrap';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import NewNote from './NewNote';
+import useLocalStorage from './useLocalStorage';
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', []);
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', []);
+
+  const notesWitheTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...notes,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/NoteForm.tsx
@@ -0,0 +1,67 @@
+import React, { FormEvent, useRef, useState } from 'react';
+import { Form, Stack, Row, Col, Button } from 'react-bootstrap';
+import CreatableReactSelect from 'react-select/creatable';
+import { Link } from 'react-router-dom';
+import { NoteData, Tag } from './App';
+
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export default function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control ref={titleRef} required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) =>
+                  tags.map((tag) => {
+                    return { label: tag.label, id: tag.value };
+                  })
+                }
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control ref={markdownRef} required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
